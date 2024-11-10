fac390abab4ccf43c4e2180a2a0b0785793b3537
WILIAM <wiliamputnomorales45@gmail.com>
2024-11-09 23:31:38 -0500

    cambios

--- calculadora/lib/main.dart
+++ calculadora/lib/main.dart
@@ -1,125 +1,213 @@
 import 'package:flutter/material.dart';
+import 'package:speech_to_text/speech_to_text.dart' as stt;
+import 'package:expressions/expressions.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: CalculatorScreen(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class CalculatorScreen extends StatefulWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _CalculatorScreenState createState() => _CalculatorScreenState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _CalculatorScreenState extends State<CalculatorScreen> {
+  late stt.SpeechToText _speech;
+  bool _isListening = false;
+  String _expression = "";
+  String _displayText = "0";
+  String _result = "";
 
-  void _incrementCounter() {
+  @override
+  void initState() {
+    super.initState();
+    _speech = stt.SpeechToText();
+  }
+
+  void _startListening() async {
+    bool available = await _speech.initialize(
+      onStatus: (val) => print('onStatus: $val'),
+      onError: (val) => print('onError: $val'),
+    );
+    if (available) {
+      setState(() => _isListening = true);
+      _speech.listen(onResult: (val) => setState(() {
+        String voiceInput = val.recognizedWords;
+        _expression = _parseVoiceInput(voiceInput);
+      }));
+    } else {
+      Fluttertoast.showToast(msg: "No se pudo iniciar el reconocimiento de voz");
+    }
+  }
+
+  void _stopListening() {
+    _speech.stop();
+    setState(() => _isListening = false);
+  }
+
+  String _parseVoiceInput(String input) {
+    return input
+        .replaceAll('por', '*')
+        .replaceAll('dividido', '/')
+        .replaceAll('más', '+')
+        .replaceAll('menos', '-')
+        .replaceAll(' ', '')
+        .replaceAll(RegExp(r'[^\d\+\-\*/\.]'), '');
+  }
+
+  void _evaluateExpression() {
+    try {
+      Expression exp = Expression.parse(_expression);
+      final evaluator = const ExpressionEvaluator();
+      double result = evaluator.eval(exp, {}) as double;
+      setState(() {
+        _result = result.toString();
+      });
+      _showResultDialog(result.toString());
+    } catch (e) {
+      setState(() {
+        _result = "Error";
+      });
+      Fluttertoast.showToast(msg: "Error en la expresión");
+    }
+  }
+
+  void _showResultDialog(String result) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Resultado'),
+          content: Text('El resultado es: $result'),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text('Cerrar'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void _buttonPressed(String buttonText) {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      if (buttonText == "CE") {
+        _expression = "";
+        _displayText = "0";
+        _result = "";
+      } else if (buttonText == "=") {
+        _evaluateExpression();
+      } else {
+        if (_expression == "0") {
+          _expression = buttonText;
+        } else {
+          _expression += buttonText;
+        }
+        _displayText = _expression;
+      }
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('Calculadora con Voz'),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/anime.jpg'), // Asegúrate de que la ruta sea correcta
+            fit: BoxFit.cover, // Hace que la imagen cubra toda la pantalla
+          ),
+        ),
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+          children: [
+            SwitchListTile(
+              title: Text('Modo de Entrada por Voz'),
+              value: _isListening,
+              onChanged: (val) {
+                if (val) {
+                  _startListening();
+                } else {
+                  _stopListening();
+                }
+              },
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            Container(
+              padding: EdgeInsets.all(20),
+              color: const Color.fromARGB(255, 246, 246, 246).withOpacity(0.6), // Fondo oscuro para el texto
+              child: Align(
+                alignment: Alignment.centerRight,
+                child: Text(
+                  _expression.isNotEmpty ? _expression : _displayText,
+                  style: TextStyle(color: Colors.blue, fontSize: 36),
+                ),
+              ),
+            ),
+            if (_result.isNotEmpty)
+              Padding(
+                padding: EdgeInsets.all(20),
+                child: Text(
+                  "Resultado: $_result",
+                  style: TextStyle(fontSize: 24, color: const Color(0xFF3E0000)),
+                ),
+              ),
+            Expanded(
+              child: GridView.count(
+                crossAxisCount: 4,
+                crossAxisSpacing: 10, // Espaciado horizontal
+                mainAxisSpacing: 10,  // Espaciado vertical
+                padding: EdgeInsets.all(10), // Espaciado externo de la cuadrícula
+                children: [
+                  ...['7', '8', '9', '/',
+                      '4', '5', '6', '*',
+                      '1', '2', '3', '-',
+                      '0', 'CE', '=', '+']
+                      .map((btnText) {
+                    return ElevatedButton(
+                      onPressed: () => _buttonPressed(btnText),
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: _getButtonColor(btnText), 
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8), // Bordes no tan circulares
+                        ),
+                        elevation: 5, // Sombra
+                      ),
+                      child: Text(
+                        btnText,
+                        style: TextStyle(fontSize: 25, color: Colors.white),
+                      ),
+                    );
+                  }),
+                ],
+              ),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+
+Color _getButtonColor(String buttonText) {
+  if (buttonText == "CE") {
+    return Colors.red; // Color rojo para el botón "CE"
+  } else if (['+', '-', '*', '/', '='].contains(buttonText)) {
+    return Colors.deepOrange; // Color para los botones de operación
+  }
+  return Colors.blue; // Color por defecto para los demás botones
+}
+
 }
